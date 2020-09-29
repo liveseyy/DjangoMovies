7f023e0a9322f34e954620ef8a800c47c2616e39
liveseyy <dima74ermilov@gmail.com>
2020-09-29 14:02:55 +0500

    added displaying movies on homepage

--- django_movie/movies/admin.py
+++ django_movie/movies/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Actor, Genre, Raiting, RatingStar, Reviews, Movie, MovieShots
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(Raiting)
+admin.site.register(RatingStar)
+admin.site.register(Reviews)
+admin.site.register(MovieShots)
+admin.site.register(Movie)
--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+import os
+
+from .models import Movie
 
 # Create your views here.
+
+
+class MoviesView(View):
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, 'movies/movie_list.html', context={'movie_list': movies})
+
