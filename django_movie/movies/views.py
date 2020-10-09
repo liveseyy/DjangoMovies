982a88f424f1773767ba149c4455bb6ea5e3043b
liveseyy <dima74ermilov@gmail.com>
2020-10-09 20:49:21 +0500

    set up the django admin panel

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -28,6 +28,7 @@ class AddReview(View):
         if form.is_valid():
             form = form.save(commit=False)
             if request.POST.get("parent", None):
+                print(request.POST)
                 form.parent_id = int(request.POST.get("parent"))
             form.movie = movie
             form.save()
