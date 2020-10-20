c715458c9627e823de8d63697c3d3bba3c9c294c
liveseyy <dima74ermilov@gmail.com>
2020-10-20 20:50:45 +0500

    added ckeditor on admin-panel for description, with youtube plugin for movie-trailer

--- django_movie/movies/admin.py
+++ django_movie/movies/admin.py
@@ -1,8 +1,18 @@
+from django import forms
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
 from .models import Category, Actor, Genre, Raiting, RatingStar, Reviews, Movie, MovieShots
 
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class MovieAdminForm(forms.ModelForm):
+    description = forms.CharField(label="Описание", widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
 # Register your models here.
 @admin.register(Category)
@@ -37,6 +47,7 @@ class MovieAdmin(admin.ModelAdmin):
     save_on_top = True
     save_as = True
     list_editable = ("draft",)
+    form = MovieAdminForm
     readonly_fields = ("get_image",)
     fieldsets = (
             (None, {
