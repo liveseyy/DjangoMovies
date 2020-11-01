86b9b1524895e601dada430fe43b291737cb0502
liveseyy <dima74ermilov@gmail.com>
2020-11-01 15:27:55 +0500

    Добавил фильтрацию по категориям и годам

--- django_movie/movies/urls.py
+++ django_movie/movies/urls.py
@@ -6,7 +6,8 @@ from . import views
 
 urlpatterns = [
     path("", views.MoviesView.as_view(), name="movie_list_url"),
+    path("filter/", views.FilterMoviesView.as_view(), name="filter_url"),
     path("<slug:slug>/", views.MovieDetailView.as_view(), name='movie_detail_url'),
     path("review/<int:pk>/", views.AddReview.as_view(), name="add_review_url"),
-    path("actor/<str:slug>/", views.ActorView.as_view(), name="actor_detail_url")
+    path("actor/<str:slug>/", views.ActorView.as_view(), name="actor_detail_url"),
 ]
--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,25 +1,31 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views import View
 import os
 
-from .models import Movie, Category, Actor
+from .models import Movie, Category, Actor, Genre
 
 from .forms import ReviewForm
-# Create your views here.
 
 
-class MoviesView(ListView):
+class GenreYear:
+    """Жанры и года выхода фильмов"""
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values_list("year", flat=True).distinct()
+
+
+
+class MoviesView(GenreYear, ListView):
     model = Movie
     queryset = Movie.objects.filter(draft=False)
 
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['categories'] = Category.objects.all()
-        return context
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     model = Movie
     slug_field = "url"
     template_name = "movies/movie_detail.html"
@@ -39,7 +45,24 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-class ActorView(DetailView):
+class ActorView(GenreYear, DetailView):
     model = Actor
     template_name = 'movies/actor.html'
     slug_field = 'name'
+
+
+class FilterMoviesView(GenreYear, ListView):
+    """Фильтр фильмов"""
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+                                        Q(year__in=self.request.GET.getlist("year")) |
+                                        Q(genres__in=self.request.GET.getlist("genre"))
+                                        ).distinct()
+        return queryset
+
+
+
+
+
+
+
