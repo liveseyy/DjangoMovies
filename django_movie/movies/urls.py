d4a9a103272f53472b470c1c87ea5b69661c36cb
liveseyy <dima74ermilov@gmail.com>
2020-12-02 13:43:27 +0500

    add search movies

--- django_movie/movies/urls.py
+++ django_movie/movies/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.MoviesView.as_view(), name="movie_list_url"),
     path("filter/", views.FilterMoviesView.as_view(), name="filter_url"),
+    path("search/", views.Search.as_view(), name='search_url'),
     path("add-rating/", views.AddStarRating.as_view(), name="add_rating_url"),
     path("<slug:slug>/", views.MovieDetailView.as_view(), name='movie_detail_url'),
     path("review/<int:pk>/", views.AddReview.as_view(), name="add_review_url"),
